24f74071d46c7bae1b27f80e7ce6034892c249d4
Jon Keon <jon.keon@gmail.com>
2020-02-28 15:17:19 -0500

    Organize Unity Update Sources in Heirarchy

--- Scripts/Runtime/DelayedExecution/UpdateSources/AbstractUnityUpdateSource.cs
+++ Scripts/Runtime/DelayedExecution/UpdateSources/AbstractUnityUpdateSource.cs
@@ -5,9 +5,26 @@ namespace Anvil.Unity.DelayedExecution
 {
     public abstract class AbstractUnityUpdateSource<T> : AbstractUpdateSource where T:AbstractUnityUpdateSourceMonoBehaviour
     {
+        private static Transform s_UpdateSourceRoot;
+
+        private static Transform UpdateSourceRoot
+        {
+            get
+            {
+                if (s_UpdateSourceRoot == null)
+                {
+                    GameObject gameObject = new GameObject("[UpdateSources]");
+                    GameObject.DontDestroyOnLoad(gameObject);
+                    s_UpdateSourceRoot = gameObject.transform;
+                }
+
+                return s_UpdateSourceRoot;
+            }
+        }
         protected override void Initialize()
         {
-            GameObject gameObject = new GameObject($"[UpdateSource - MonoBehaviour - {typeof(T)}]");
+            GameObject gameObject = new GameObject($"[UpdateSource - {typeof(T)}]");
+            gameObject.transform.SetParent(UpdateSourceRoot);
             GameObject.DontDestroyOnLoad(gameObject);
             T updateSource = gameObject.AddComponent<T>();
             updateSource.SetOnUpdateDispatchFunction(DispatchOnUpdateEvent);
