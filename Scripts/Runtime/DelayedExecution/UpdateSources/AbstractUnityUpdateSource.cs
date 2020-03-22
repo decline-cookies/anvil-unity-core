a00d249936a0c6941722542e4d0ec6d3038f66dc
Jon Keon <jon.keon@gmail.com>
2020-03-21 21:05:22 -0400

    Fixing a bug where multiple UpdateHandles of different sources wouldn't get grouped under the same root

--- Scripts/Runtime/DelayedExecution/UpdateSources/AbstractUnityUpdateSource.cs
+++ Scripts/Runtime/DelayedExecution/UpdateSources/AbstractUnityUpdateSource.cs
@@ -5,29 +5,33 @@ namespace Anvil.Unity.DelayedExecution
 {
     public abstract class AbstractUnityUpdateSource<T> : AbstractUpdateSource where T:AbstractUnityUpdateSourceMonoBehaviour
     {
+        protected override void Init()
+        {
+            GameObject gameObject = new GameObject($"[UpdateSource - {typeof(T)}]");
+            GameObject.DontDestroyOnLoad(gameObject);
+            gameObject.transform.SetParent(UnityUpdateSourceGameObjectManager.UpdateSourceRoot);
+            T updateSource = gameObject.AddComponent<T>();
+            updateSource.SetOnUpdateDispatchFunction(DispatchOnUpdateEvent);
+        }
+    }
+    
+    internal static class UnityUpdateSourceGameObjectManager
+    {
+        private const string UPDATE_SOURCES_GAME_OBJECT_NAME = "[UpdateSources]";
         private static Transform s_UpdateSourceRoot;
-
-        private static Transform UpdateSourceRoot
+        public static Transform UpdateSourceRoot
         {
             get
             {
                 if (s_UpdateSourceRoot == null)
                 {
-                    GameObject gameObject = new GameObject("[UpdateSources]");
+                    GameObject gameObject = new GameObject(UPDATE_SOURCES_GAME_OBJECT_NAME);
                     GameObject.DontDestroyOnLoad(gameObject);
                     s_UpdateSourceRoot = gameObject.transform;
                 }
 
                 return s_UpdateSourceRoot;
             }
-        }
-        protected override void Init()
-        {
-            GameObject gameObject = new GameObject($"[UpdateSource - {typeof(T)}]");
-            GameObject.DontDestroyOnLoad(gameObject);
-            gameObject.transform.SetParent(UpdateSourceRoot);
-            T updateSource = gameObject.AddComponent<T>();
-            updateSource.SetOnUpdateDispatchFunction(DispatchOnUpdateEvent);
         }
     }
 }
