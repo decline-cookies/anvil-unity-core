b6fb742681d42fb729448f394fa9f834ff0acc1b
Jon Keon <jon.keon@gmail.com>
2020-04-15 12:36:26 -0400

    WIP

---
+++ Scripts/Runtime/Content/AbstractUnityContentController.cs
@@ -0,0 +1,41 @@
+﻿using Anvil.CSharp.Content;
+using UnityEngine;
+
+namespace Anvil.Unity.Content
+{
+    public abstract class AbstractUnityContentController : AbstractContentController
+    {
+        
+        //TODO: Remove later on
+        private ResourceRequest m_ResourceRequest;
+        protected AbstractUnityContentController(string contentGroupID, string contentLoadingID) : base(contentGroupID, contentLoadingID)
+        {
+        }
+
+        public override void Load()
+        {
+            //TODO: Need to load all the required assets.
+            //TODO: Need to load the actual prefab or scene.
+            //TODO: Need to allow the user to insert their own logic in here.
+            
+            //For now we'll just assume it's a prefab and we're Resources.Loading it.
+            //TODO: Support addressables
+
+            m_ResourceRequest = Resources.LoadAsync<GameObject>(ContentLoadingID);
+            m_ResourceRequest.completed += HandleOnResourceLoaded;
+        }
+        
+        private void HandleOnResourceLoaded(AsyncOperation asyncOperation)
+        {
+            GameObject instance = (GameObject)GameObject.Instantiate(m_ResourceRequest.asset);
+            //TODO: Properly sanitize the name with a Regex via util method
+            instance.name = instance.name.Replace("(Clone)", string.Empty);
+            Content = instance.GetComponent<IContent>();
+            Content.Controller = this;
+            
+            m_ResourceRequest.completed -= HandleOnResourceLoaded;
+            OnLoadComplete?.Invoke();
+        }
+    }
+}
+
---
+++ Scripts/Runtime/Content/UnityContentManager.cs
@@ -0,0 +1,44 @@
+﻿using Anvil.CSharp.Content;
+using UnityEngine;
+
+namespace Anvil.Unity.Content
+{
+    public class UnityContentManager : AbstractContentManager<UnityContentGroup,AbstractUnityContentController,AbstractUnityContent>
+    {
+        public readonly Transform ContentRoot;
+
+        public UnityContentManager(Transform contentRoot) : base()
+        {
+            ContentRoot = contentRoot;
+        }
+
+        protected override UnityContentGroup ConstructContentGroup(string id, Vector3 localPosition, Transform gameObjectRoot = null)
+        {
+            return new UnityContentGroup(this, id, localPosition, gameObjectRoot);
+        }
+
+        protected override void LogWarning(string msg)
+        {
+            Debug.LogWarning(msg);
+        }
+
+
+        public AbstractContentManager CreateContentGroup(string id, Vector3 localPosition, Transform gameObjectRoot = null)
+        {
+            if (m_ContentGroups.ContainsKey(id))
+            {
+                throw new ArgumentException($"Content Groups ID of {id} is already registered with the Content Manager!");
+            }
+            
+            AbstractContentGroup contentGroup = new AbstractContentGroup(this, id, localPosition, gameObjectRoot);
+            m_ContentGroups.Add(contentGroup.ID, contentGroup);
+
+            AddLifeCycleListeners(contentGroup);
+            
+            return this;
+        }
+        
+        
+    }
+}
+
