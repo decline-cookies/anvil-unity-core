a8b466ccd732453b4b005c9110f616d97ef72c8d
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2020-04-20 00:15:21 -0400

    Implement UnityEditorUpdateSource
     - Allows UpdateHandles that are driven by the EditorApplication.Update event

---
+++ Scripts/Editor/DelayedExecution/UpdateSource/UnityEditorUpdateSource.cs
@@ -0,0 +1,25 @@
+﻿using Anvil.CSharp.DelayedExecution;
+using UnityEditor;
+
+namespace Anvil.UnityEditor.DelayedExecution
+{
+    public class UnityEditorUpdateSource : AbstractUpdateSource
+    {
+        protected override void Init()
+        {
+            EditorApplication.update += EditorApplication_update;
+        }
+
+        protected override void DisposeSelf()
+        {
+            EditorApplication.update -= EditorApplication_update;
+
+            base.DisposeSelf();
+        }
+
+        private void EditorApplication_update()
+        {
+            DispatchOnUpdateEvent();
+        }
+    }
+}
