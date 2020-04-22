f0177dce8da2e24727b98511b0483d3923237b41
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2020-04-22 14:39:17 -0400

    UnityEditorUpdateSource - Document class

--- Scripts/Editor/DelayedExecution/UpdateSource/UnityEditorUpdateSource.cs
+++ Scripts/Editor/DelayedExecution/UpdateSource/UnityEditorUpdateSource.cs
@@ -3,6 +3,10 @@ using UnityEditor;
 
 namespace Anvil.UnityEditor.DelayedExecution
 {
+    /// <summary>
+    /// An update source that hooks into the <see cref="EditorApplication.update"/> event
+    /// and provides it to a <see cref="UpdateHandle"/>
+    /// </summary>
     public class UnityEditorUpdateSource : AbstractUpdateSource
     {
         protected override void Init()
