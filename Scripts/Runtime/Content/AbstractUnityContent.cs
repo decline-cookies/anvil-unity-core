73cd7102a0df951ea1091c9efd275d7ab1a5538c
Jon Keon <jon.keon@gmail.com>
2020-04-21 21:34:35 -0400

    PR Fixes

--- Scripts/Runtime/Content/AbstractUnityContent.cs
+++ Scripts/Runtime/Content/AbstractUnityContent.cs
@@ -1,9 +1,22 @@
-﻿using System;
-using Anvil.CSharp.Content;
+﻿using Anvil.CSharp.Content;
 using Anvil.Unity.Core;
 
 namespace Anvil.Unity.Content
 {
+    /// <summary>
+    /// Convenience class to allow <see cref="AbstractUnityContentController"/> corresponding to <see cref="AbstractUnityContent"/> to be specific
+    /// and strongly typed.
+    /// </summary>
+    /// <typeparam name="TController">A specific implementation of <see cref="AbstractUnityContent"/>.</typeparam>
+    public abstract class AbstractUnityContent<TController> : AbstractUnityContent
+        where TController : AbstractContentController
+    {
+        /// <summary>
+        /// Gets/Sets the corresponding Controller as strongly typed version of <see cref="AbstractUnityContentController"/>
+        /// </summary>
+        public new TController Controller { get; internal set; }
+    }
+    
     /// <summary>
     /// A more specific implementation of <see cref="IContent"/> for usage with Unity where the Content is a
     /// <see cref="AbstractAnvilMonoBehaviour"/>
@@ -11,10 +24,10 @@ namespace Anvil.Unity.Content
     public abstract class AbstractUnityContent : AbstractAnvilMonoBehaviour, IContent
     {
         /// <summary>
-        /// <inheritdoc cref="IContent.OnContentDisposing"/>
+        /// <inheritdoc cref="IContent.Controller"/>
         /// </summary>
-        public event Action OnContentDisposing;
-        
+        public AbstractContentController Controller { get; internal set; }
+
         private bool m_IsContentDisposing;
 
         protected override void DisposeSelf()
@@ -25,8 +38,8 @@ namespace Anvil.Unity.Content
             }
             m_IsContentDisposing = true;
             
-            OnContentDisposing?.Invoke();
-            OnContentDisposing = null;
+            Controller?.Dispose();
+            Controller = null;
 
             base.DisposeSelf();
         }
