5958f06d716176d199a32457b4e0ebab45d526ad
Jon Keon <jon.keon@gmail.com>
2020-05-13 12:46:10 -0400

    Update Handle Refactor

---
+++ Scripts/Runtime/DelayedExecution/CallLaterHandles/UnityDeltaProviders.cs
@@ -0,0 +1,14 @@
+﻿using Anvil.CSharp.DelayedExecution;
+using UnityEngine;
+
+namespace Anvil.Unity.DelayedExecution
+{
+    public static class UnityDeltaProviders
+    {
+        public static DeltaFramesProvider Frames = () => 1;
+        public static DeltaTimeProvider DeltaTime = () => Time.deltaTime;
+        public static DeltaTimeProvider UnscaledDeltaTime = () => Time.unscaledDeltaTime;
+        public static DeltaTimeProvider FixedDeltaTime = () => Time.fixedDeltaTime;
+        public static DeltaTimeProvider UnscaledFixedDeltaTime = () => Time.fixedUnscaledDeltaTime;
+    }
+}
