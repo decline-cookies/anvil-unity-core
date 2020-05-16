a7dda45dc185221dd1ce3845206b42471c5c497d
Jon Keon <jon.keon@gmail.com>
2020-05-15 20:38:50 -0400

    Initial proof of concept

---
+++ Scripts/Editor/Project/JSONSetup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using Anvil.CSharp.Data;
+using Anvil.Unity.Data;
+using UnityEditor;
+
+namespace Anvil.UnityEditor.Project
+{
+    /// <summary>
+    /// Editor helper to redirect <see cref="Console.WriteLine"/> and related functions to output in the Unity Console
+    /// Window.
+    /// </summary>
+    [InitializeOnLoad]
+    public class JSONSetup
+    {
+        static JSONSetup()
+        {
+            JSON.OverrideInstance(new UnityTinyJSONWorker());
+        }
+    }
+}
