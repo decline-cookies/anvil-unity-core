8f225493620d88269ddc94e98dad2f3ef27ce818
Jon Keon <jon.keon@gmail.com>
2020-06-10 14:24:56 -0400

    Bug fixes and defaults

---
+++ Scripts/Editor/Data/Serialization/UnityOverrideJSONParser.cs
@@ -0,0 +1,14 @@
+﻿using Anvil.Unity.Data;
+using UnityEditor;
+
+namespace Anvil.UnityEditor.Data
+{
+    [InitializeOnLoad]
+    public class UnityOverrideJSONParser
+    {
+        static UnityOverrideJSONParser()
+        {
+            UnityTinyJSONParser.Register();
+        }
+    }
+}
---
+++ Scripts/Runtime/Data/Serialization/TinyJSON/UnityDecoder.cs
@@ -0,0 +1,8 @@
+﻿using TinyJSON;
+
+namespace Anvil.Unity.Data
+{
+    public class UnityDecoder : Decoder<ProxyArray, ProxyBoolean, ProxyNumber, ProxyObject, UnityProxyString>
+    {
+    }
+}
