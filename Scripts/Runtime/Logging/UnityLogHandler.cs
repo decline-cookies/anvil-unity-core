1405ad8ce2762fa1efdec09ca00fec9fc67b067a
Tyler Vezina <tylerjvezina@gmail.com>
2020-09-22 10:18:20 -0400

    First pass of UnityLogHandler

---
+++ Scripts/Runtime/Logging/UnityLogHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Anvil.CSharp.Logging;
+using UnityEngine;
+using ILogHandler = Anvil.CSharp.Logging.ILogHandler;
+
+namespace Anvil.Unity.Logging
+{
+    public class UnityLogHandler : ILogHandler
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+        private static void RegisterDefaultLogHandler() => Log.OverrideDefaultHandler(new UnityLogHandler());
+
+        public void HandleLog(LogLevel level, string message)
+        {
+            switch (level)
+            {
+                case LogLevel.Debug:
+                    Debug.Log(message);
+                    break;
+                case LogLevel.Warning:
+                    Debug.LogWarning(message);
+                    break;
+                case LogLevel.Error:
+                    Debug.LogError(message);
+                    break;
+                default:
+                    throw new ArgumentException($"Unhandled log level: {level}");
+            }
+        }
+    }
+}
