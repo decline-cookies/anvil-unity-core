d547ae5a8f5390adebb31aad56f9a295b347a99e
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2022-07-15 12:41:49 -0400

    Log - Add StateResetHelper
    Resets the static state of the CSharp side of the log system when domain reload is disabled in Unity.

---
+++ Scripts/Runtime/Logging/.CSProject/StaticStateResetHelper.cs
@@ -0,0 +1,18 @@
+using Anvil.CSharp.Logging;
+using UnityEngine;
+
+namespace Anvil.Unity.Logging
+{
+    /// <summary>
+    /// Hooks into <see cref="RuntimeInitializeOnLoadMethod"/> to reset static Log state
+    /// when projects don't perform domain reloads between runs.
+    /// </summary>
+    internal class StaticStateResetHelper
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void Init()
+        {
+            OneTimeLogger.Reset();
+        }
+    }
+}
