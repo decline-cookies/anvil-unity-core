472e91098630cd5a8c8dea724add572c5a6d4add
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2022-12-02 23:12:50 -0500

    MathExtensionTests - Stub out

---
+++ Scripts/Editor/Tests/Core/Util/MathExtensionTests.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+
+namespace Anvil.Unity.Tests
+{
+    public static class MathExtensionTests
+    {
+        [Test]
+        public static void GetInverseTest()
+        {
+
+        }
+
+        [Test]
+        public static void GetInverseSafe()
+        {
+
+        }
+
+        [Test]
+        public static void IsApproximately()
+        {
+
+        }
+
+        [Test]
+        public static void ToSignedInfinite()
+        {
+
+        }
+    }
+}
