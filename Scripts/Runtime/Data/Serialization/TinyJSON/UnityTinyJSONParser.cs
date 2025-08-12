1e87be15f3d0f0806caa9f6f8fc7a02cc208c7a5
Jon Keon <339008+jkeon@users.noreply.github.com>
2025-08-12 12:38:26 -0400

    Adding support for Vector3 and Quaternion

--- Scripts/Runtime/Data/Serialization/TinyJSON/UnityTinyJSONParser.cs
+++ Scripts/Runtime/Data/Serialization/TinyJSON/UnityTinyJSONParser.cs
@@ -31,7 +31,10 @@ namespace Anvil.Unity.Data
 
         protected override bool DTCheckType<T>(Variant data, Type type, out T decodedData)
         {
+            //TODO: This should be a mapping of type to function
             return base.DTCheckType(data, type, out decodedData)
+                || DTCheckVector3(data, type, out decodedData)
+                || DTCheckQuaternion(data, type, out decodedData)
                 || DTCheckVector2Int(data, type, out decodedData)
                 || DTCheckVector3Int(data, type, out decodedData);
         }
@@ -55,6 +58,30 @@ namespace Anvil.Unity.Data
             {
                 Vector3 value = DecodeType<Vector3>(data);
                 decodedType = (T)(object)new Vector3Int(Mathf.RoundToInt(value.x), Mathf.RoundToInt(value.y), Mathf.RoundToInt(value.z));
+                return true;
+            }
+
+            decodedType = default;
+            return false;
+        }
+
+        private bool DTCheckVector3<T>(Variant data, Type type, out T decodedType)
+        {
+            if (typeof(Vector3).IsAssignableFrom(type))
+            {
+                decodedType = (T)(object)new Vector3(data["x"], data["y"], data["z"]);
+                return true;
+            }
+
+            decodedType = default;
+            return false;
+        }
+
+        private bool DTCheckQuaternion<T>(Variant data, Type type, out T decodedType)
+        {
+            if (typeof(Quaternion).IsAssignableFrom(type))
+            {
+                decodedType = (T)(object)new Quaternion(data["x"], data["y"], data["z"], data["w"]);
                 return true;
             }
 
