e249b380d8dec4b5d57d3976e97fb9ad58b6dd59
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2025-11-14 10:53:50 -0500

    ContentManagement - Add methods to load content
     - Provide convenience methods to load content through Resources or Scenes
     - Automatically track and unload scenes when the content controller is disposed.

--- Scripts/Runtime/Content/AbstractUnityContentController.cs
+++ Scripts/Runtime/Content/AbstractUnityContentController.cs
@@ -1,8 +1,13 @@
 using System;
+using System.Collections.Generic;
 using Anvil.CSharp.Content;
+using Anvil.CSharp.Logging;
 using Anvil.Unity.Asset;
+using Anvil.Unity.Core;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+using Cysharp.Threading.Tasks;
 
 namespace Anvil.Unity.Content
 {
@@ -19,6 +24,9 @@ namespace Anvil.Unity.Content
         private LoadResourceCommand<GameObject> m_LoadResourceCommand;
         private string m_TempContentLoadingID;
 
+        private readonly List<Scene> m_LoadedContentScenes = new List<Scene>();
+
+
         protected AbstractUnityContentController(string contentGroupID)
             :base(contentGroupID) { }
 
@@ -31,6 +39,10 @@ namespace Anvil.Unity.Content
 
         protected override void DisposeSelf()
         {
+            UnloadAllContentScenesAsync()
+                .GetAwaiter()
+                .GetResult();
+
             m_LoadResourceCommand?.Dispose();
             m_LoadResourceCommand = null;
 
@@ -61,11 +73,72 @@ namespace Anvil.Unity.Content
             m_LoadResourceCommand.Dispose();
             m_LoadResourceCommand = null;
 
-            //TODO: Properly sanitize the name with a Regex via util method
-            instance.name = instance.name.Replace("(Clone)", string.Empty);
+            instance.RemoveCloneSuffix();
             TContent content = instance.GetComponent<TContent>();
 
             LoadComplete(content);
+        }
+
+        /// <summary>
+        /// Async load a given instance of <see cref="TContent"/> from a <see cref="Resources"/> path.
+        /// </summary>
+        /// <param name="path">The path to the resource.</param>
+        /// <returns>An async task that, when complete, provides an instance to <see cref="TContent"/>.</returns>
+        protected async UniTask<TContent> LoadContentAsResourceAsync(string path)
+        {
+            TContent template = (TContent)await Resources.LoadAsync<TContent>(path);
+            TContent content = GameObject.Instantiate(template);
+            content.RemoveCloneSuffix();
+
+            return content;
+        }
+
+        /// <summary>
+        /// Async load a given scene and fetch the <see cref="TContent"/> instance from one of the scene's <see cref="GameObject"/>s.
+        /// </summary>
+        /// <param name="sceneName">The scene name to load.</param>
+        /// <param name="mode">The scene loading mode to use.</param>
+        /// <param name="setSceneActive">
+        /// Whether to set the loaded scene as active. This parameter has no affect and true is implied when
+        /// <see cref="mode"/>==<see cref="LoadSceneMode.Single"/>.
+        /// If a scene is set to active it controls the environment (skybox, baked light, etc..) <see cref="SceneManager.SetActiveScene"/>.
+        /// </param>
+        /// <returns>An async task that, when complete, provides an instance to <see cref="TContent"/> from the scene.</returns>
+        /// <exception cref="Exception">Thrown if an instance of <see cref="TContent"/> can't be found in the scene.</exception>
+        /// <remarks>Loaded scenes will be automatically unloaded when the content is disposed.</remarks>
+        protected async UniTask<TContent> LoadContentAsSceneAsync(
+            string sceneName,
+            LoadSceneMode mode = LoadSceneMode.Single,
+            bool setSceneActive = true)
+        {
+            await SceneManager.LoadSceneAsync(sceneName, mode);
+
+            // If the scene wasn't loaded it'll be invalid. This should never happen because we just loaded the scene
+            // with the same path above.
+            Scene scene = SceneManager.GetSceneByName(sceneName);
+            Debug.Assert(scene.IsValid());
+            m_LoadedContentScenes.Add(scene);
+
+            if (setSceneActive)
+            {
+                // Set the scene as active so it controls the environment (skybox, baked light, etc..)
+                SceneManager.SetActiveScene(scene);
+            }
+
+            if(!scene.TryFindObjectByType(out TContent content, true))
+            {
+                throw new Exception($"{typeof(TContent).GetReadableName()} is missing from loaded scene. Scene:{sceneName}");
+            }
+
+            return content;
+        }
+
+        private async UniTask UnloadAllContentScenesAsync()
+        {
+            await m_LoadedContentScenes
+                .Select(scene => SceneManager.UnloadSceneAsync(scene).ToUniTask());
+
+            m_LoadedContentScenes.Clear();
         }
     }
 }
